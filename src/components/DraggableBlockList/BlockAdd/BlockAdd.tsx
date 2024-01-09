feddafefc779cf20ff0ad8c8009e06aac611b73f
Morghot2 <maximilian.jaron@gmail.com>
2024-01-09 11:37:58 +0100

    DG-000003: logic for adding, removing and dragging

--- src/components/DraggableBlockList/BlockAdd/BlockAdd.tsx
+++ src/components/DraggableBlockList/BlockAdd/BlockAdd.tsx
@@ -1,10 +1,36 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { v4 as uuidv4 } from 'uuid';
 import { TextInput, TextInputWrapper, AddButton } from './BlockAdd.styles';
+import { BlockAddProps } from '../DraggableBlockList.utils';
 
-export const BlockAdd = () => {
+export const BlockAdd = ({
+  blocks,
+  setBlocks,
+  inputContent,
+  setInputContent,
+}: BlockAddProps) => {
+  const handleAddBlock = () => {
+    if (inputContent.trim() === '') {
+      return;
+    }
+    const newBlock = {
+      id: uuidv4(),
+      name: inputContent,
+    };
+
+    setBlocks([...blocks, newBlock]);
+    setInputContent('');
+  };
   return (
     <TextInputWrapper>
-      <TextInput type="text" placeholder="Name block" />
-      <AddButton onClick={() => console.log('clicked')}>Add Item</AddButton>
+      <TextInput
+        type="text"
+        placeholder="Name block"
+        value={inputContent}
+        onChange={e => setInputContent(e.target.value)}
+      />
+      <AddButton onClick={handleAddBlock}>Add Item</AddButton>
     </TextInputWrapper>
   );
 };
--- src/components/DraggableBlockList/DraggableBlockList.tsx
+++ src/components/DraggableBlockList/DraggableBlockList.tsx
@@ -1,14 +1,22 @@
+import { useState } from 'react';
 import { DraggableBlockListWrapper, Title } from './DraggableBlockList.styles';
 import { initialBlockList } from './DraggableBlockList.utils';
 import { Blocks } from './Blocks/Blocks';
 import { BlockAdd } from './BlockAdd/BlockAdd';
 
 export const DraggableBlockList = () => {
+  const [blocks, setBlocks] = useState(initialBlockList);
+  const [inputContent, setInputContent] = useState('');
   return (
     <DraggableBlockListWrapper>
       <Title>Draggame</Title>
-      <BlockAdd />
-      <Blocks initialBlocks={initialBlockList} />
+      <BlockAdd
+        blocks={blocks}
+        setBlocks={setBlocks}
+        inputContent={inputContent}
+        setInputContent={setInputContent}
+      />
+      <Blocks blocks={blocks} setBlocks={setBlocks} />
     </DraggableBlockListWrapper>
   );
 };
